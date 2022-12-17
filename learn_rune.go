c46c6b69b2804ffbbd33294a0fe8da713b1aca62
David Hull <davidhullster@gmail.com>
2022-12-17 17:52:46 +0000

    add string and rune methods

---
+++ learn_rune.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// single-quotes indicate rune
+// single-quotes around multiple characters is an error
+func main() {
+	fmt.Println('L') // outputs ordinal for letter/rune
+	//fmt.Println('Less is More')
+}
---
+++ learn_string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Simple String")
+	fmt.Println(`
+	This is a multi-line
+	String that can contain "quotes".
+	`)
+		fmt.Println("")
+		fmt.Println("\u2272")
+}
