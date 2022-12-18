0e1ccea1c43516f4f0350c134bccfe0b235a9b37
David Hull <davidhullster@gmail.com>
2022-12-18 05:11:41 +0000

    add booleans basics

---
+++ learn_booleans.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Greater Than: (1 > 2)", 1 > 2)
+	fmt.Println("Less Than: (1 < 2)", 1 < 2)
+	fmt.Println("Less Than or Equal To: (1 <= 2)", 1 <= 2)
+	fmt.Println("Greater Than or Equal To: (1 >= 2)", 1 >= 2)
+	fmt.Println("Equivalent: (4.0 == 4)", 4.0 == 4)
+	fmt.Println("Not Equivalent: (4.0 != 4.1)", 4.0 != 4.1)
+}
