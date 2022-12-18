797a8ea98f4330bbcac5cb7f0875fb75c2c66256
David Hull <davidhullster@gmail.com>
2022-12-18 05:12:47 +0000

    add floats and exponents (math.Pow())

--- learn_numbers.go
+++ learn_numbers.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+  )
 
 func main() {
 	fmt.Println("Addition:", 1+3)
 	fmt.Println("Subtraction:", 27-19)
 	fmt.Println("Multiplication:", 9*11)
 	fmt.Println("Division:", 20/4)
+	fmt.Println("Division:", 20.0/3)
+	fmt.Println("Exponents:", math.Pow(20.0,3))
 }
