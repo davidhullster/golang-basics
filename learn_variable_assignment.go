95b22820a3acdded45e9aaa25fdd65cc3c6a275a
David Hull <davidhullster@gmail.com>
2022-12-18 05:32:37 +0000

    demonstrate variable assignment with :=

---
+++ learn_variable_assignment.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	theInt := 16
+	name = "Tablature"
+
+	fmt.Println("name:", name)
+	fmt.Println("theInt:", theInt)
+	fmt.Println("theInt times 2:", theInt*2)
+}
